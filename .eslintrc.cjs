f9997eb92f8715e563f11dd1a6661329e96d2955
lerrybe <lerry597@gmail.com>
2023-07-26 19:56:56 +0900

    chore: modify format style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
-      'warn',
       { allowConstantExport: true },
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',

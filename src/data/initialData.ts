281c7d8516822d0a580bea6bc28151be101ff4f6
lerrybe <lerry597@gmail.com>
2023-07-26 19:58:54 +0900

    Add initial Data for dotting and yorkie

---
+++ src/data/initialData.ts
@@ -0,0 +1,20 @@
+export const initialDataArray = Array(30)
+  .fill('')
+  .map((_, rowIndex) => {
+    return Array(30)
+      .fill('')
+      .map((_, columnIndex) => {
+        return {
+          color: '',
+          rowIndex,
+          columnIndex,
+        };
+      });
+  });
+
+export const initialIndices = {
+  topRowIndex: 0,
+  bottomRowIndex: 30,
+  leftColumnIndex: 0,
+  rightColumnIndex: 30,
+};

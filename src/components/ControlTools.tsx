43309247357966c3f139c3f12babd5614efdf248
lerrybe <lerry597@gmail.com>
2023-08-20 07:54:14 +0900

    Implement Toolbar

---
+++ src/components/ControlTools.tsx
@@ -0,0 +1,63 @@
+import React, { ForwardedRef, forwardRef, useEffect } from 'react';
+import { DottingRef, useDotting } from 'dotting';
+
+import Tool from './Tool.js';
+import { FaRegTrashAlt } from 'react-icons/fa';
+import { BiRedo, BiUndo } from 'react-icons/bi';
+import { MdOutlineAddPhotoAlternate } from 'react-icons/md';
+import { TOOL_GIRD_CLASS, TOOL_TEMPLATE_CLASS } from '../styles/styleClass.js';
+
+const ControlTools = forwardRef(function InnerToolbar({}, ref: ForwardedRef<DottingRef>) {
+  // @ts-ignore
+  const { undo, redo, clear } = useDotting(ref);
+
+  useEffect(() => {
+    const keyboardUndoListener = (e: KeyboardEvent) => {
+      if (e.code === 'KeyZ' && (e.ctrlKey || e.metaKey)) {
+        undo();
+      }
+    };
+    const keyboardRedoListener = (e: KeyboardEvent) => {
+      if (e.code === 'KeyR' && (e.ctrlKey || e.metaKey)) {
+        redo();
+      }
+    };
+    document.addEventListener('keydown', keyboardUndoListener);
+    document.addEventListener('keydown', keyboardRedoListener);
+    return () => {
+      document.removeEventListener('keydown', keyboardUndoListener);
+      document.removeEventListener('keydown', keyboardRedoListener);
+    };
+  }, []);
+
+  return (
+    <div className={`${TOOL_TEMPLATE_CLASS} ${TOOL_GIRD_CLASS}`}>
+      <Tool
+        handleClick={() => {
+          console.log('Add image to pixelate');
+        }}
+      >
+        <MdOutlineAddPhotoAlternate color={'#000'} size={18} />
+      </Tool>
+      <Tool handleClick={clear}>
+        <FaRegTrashAlt color={'#000'} size={14} />
+      </Tool>
+      <Tool
+        handleClick={() => {
+          console.log('global undo');
+        }}
+      >
+        <BiUndo color={'#000'} size={20} />
+      </Tool>
+      <Tool
+        handleClick={() => {
+          console.log('global redo');
+        }}
+      >
+        <BiRedo color={'#000'} size={20} />
+      </Tool>
+    </div>
+  );
+});
+
+export default ControlTools;
---
+++ src/components/PaintTools.tsx
@@ -0,0 +1,52 @@
+import React, { ForwardedRef, forwardRef } from 'react';
+import { BrushTool, DottingRef, useBrush } from 'dotting';
+
+import Tool from './Tool.js';
+import { BiSolidSquare } from 'react-icons/bi';
+import { PiSelectionPlusBold } from 'react-icons/pi';
+import { LuEraser, LuPaintBucket } from 'react-icons/lu';
+import { TOOL_GIRD_CLASS, TOOL_TEMPLATE_CLASS } from '../styles/styleClass.js';
+
+const PaintTools = forwardRef(function InnerToolbar({}, ref: ForwardedRef<DottingRef>) {
+  // @ts-ignore
+  const { changeBrushTool, brushTool } = useBrush(ref);
+
+  return (
+    <div className={`${TOOL_TEMPLATE_CLASS} ${TOOL_GIRD_CLASS}`}>
+      <Tool
+        selected={brushTool === BrushTool.DOT}
+        handleClick={() => {
+          changeBrushTool(BrushTool.DOT);
+        }}
+      >
+        <BiSolidSquare size={10} color={brushTool === BrushTool.DOT ? '#fff' : '#000'} />
+      </Tool>
+      <Tool
+        selected={brushTool === BrushTool.ERASER}
+        handleClick={() => {
+          changeBrushTool(BrushTool.ERASER);
+        }}
+      >
+        <LuEraser color={brushTool === BrushTool.ERASER ? '#fff' : '#000'} />
+      </Tool>
+      <Tool
+        selected={brushTool === BrushTool.PAINT_BUCKET}
+        handleClick={() => {
+          changeBrushTool(BrushTool.PAINT_BUCKET);
+        }}
+      >
+        <LuPaintBucket color={brushTool === BrushTool.PAINT_BUCKET ? '#fff' : '#000'} />
+      </Tool>
+      <Tool
+        selected={brushTool === BrushTool.SELECT}
+        handleClick={() => {
+          changeBrushTool(BrushTool.SELECT);
+        }}
+      >
+        <PiSelectionPlusBold color={brushTool === BrushTool.SELECT ? '#fff' : '#000'} />
+      </Tool>
+    </div>
+  );
+});
+
+export default PaintTools;

451e6bc88f7bb5cf2833c3247d0a9109366ba1b8
lerrybe <lerry597@gmail.com>
2023-08-20 07:53:11 +0900

    Add Dotting context with contextAPI

---
+++ src/context/DottingContext.tsx
@@ -0,0 +1,44 @@
+import React, { createContext, useContext } from 'react';
+
+type DottingContextType = {
+  isGridFixed: boolean;
+  isGridVisible: boolean;
+  isPanZoomEnable: boolean;
+  setIsGridFixed: (isGridFixed: boolean) => void;
+  setIsGridVisible: (isGridVisible: boolean) => void;
+  setIsPanZoomEnable: (isPanZoomEnable: boolean) => void;
+};
+
+const DottingContext = createContext<DottingContextType | null>({
+  isGridFixed: false,
+  isGridVisible: true,
+  isPanZoomEnable: true,
+  setIsGridFixed: (isGridFixed: boolean) => {},
+  setIsGridVisible: (isGridVisible: boolean) => {},
+  setIsPanZoomEnable: (isPanZoomEnable: boolean) => {},
+});
+
+function DottingProvider({ children }: { children: React.ReactNode }) {
+  const [isGridFixed, setIsGridFixed] = React.useState(false);
+  const [isGridVisible, setIsGridVisible] = React.useState(true);
+  const [isPanZoomEnable, setIsPanZoomEnable] = React.useState(true);
+
+  return (
+    <DottingContext.Provider
+      value={{
+        isGridFixed,
+        isGridVisible,
+        isPanZoomEnable,
+        setIsGridFixed,
+        setIsGridVisible,
+        setIsPanZoomEnable,
+      }}
+    >
+      {children}
+    </DottingContext.Provider>
+  );
+}
+
+export const useDottingContext = () => useContext(DottingContext);
+
+export default DottingProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./styles/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import 'flowbite';
+import './styles/index.css';
+import App from './App.tsx';
+import DottingProvider from './context/DottingContext.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <DottingProvider>
+      <App />
+    </DottingProvider>
+  </React.StrictMode>,
 );

8cc9ff470150724556ed42db67a7422ccc6d0c34
lerrybe <lerry597@gmail.com>
2023-09-06 23:56:41 +0900

    chore: change base url for deploy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Document from '../pages/Document';
+import { PUBLIC_URL } from '../constant/urls';
 
 export default function Routing() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/:docId' element={<Document />} />
